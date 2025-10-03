ffc96e4b6c5bdd81bbe8875f08e71cd55caec242
Venkata <kopparapu.v@northeastern.edu>
2025-10-03 11:35:39 -0700

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -2,7 +2,7 @@
 # Brute force approach
 
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self,nums,target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
