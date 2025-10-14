f8a64f2dd49e9f53b8d5ef441f577a77864d3c9d
Venkata <kopparapu.v@northeastern.edu>
2025-10-14 10:25:22 -0700

    Contains_Duplicate.py

--- Contains_Duplicate.py
+++ Contains_Duplicate.py
@@ -1,6 +1,43 @@
 LeetCode 217 – Contains Duplicate
 # Brute Force Approach
 
+## Problem
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+## Solution 
+
 class Solution:
     def containsDuplicate(self, nums):
         for i in range(len(nums)):              # Loop over each element
@@ -14,7 +51,7 @@ Time Complexity
 O(n²) → Because for each element, we check all other elements after it.
 
   
-Problem Explanation
+##  Problem Explanation
 Given an array of integers nums, return True if any value appears at least twice in the array, and False if every element is unique.
 Input: nums = [1, 2, 3, 1]
 Output: True → because 1 appears more than once.
